c04988f3394cc56510e07c06ba3cad30178cb606
Raznan-Ahamed <raznanahamed3@gmail.com>
2023-08-14 08:16:08 +0530

    Fix FileNotFoundError

--- bootloader.py
+++ bootloader.py
@@ -3,15 +3,17 @@ import subprocess, shlex, os
 def set_bootloader_password(password, re_password, username):
 	# Generate Password Hash
 	input_data = f"{password}\n{re_password}"
-	process = subprocess.run(['grub-mkpasswd-pbkdf2'], capture_output=True, text=True, input=input_data)
+	process = subprocess.run(['grub-mkpasswd-pbkdf2'], capture_output=True, text=True, input=input_data, shell=True)
 	password_hash = process.stdout.split()
 
 	# Set Bootloader Username and Password
-	command_echo = f'sudo echo "set superusers={username}"  | tee -a /etc/grub.d/40_custom && echo "password_pbkdf2 {username} {password_hash[10]}"  | tee -a /etc/grub.d/40_custom'
-	process_echo = subprocess.run(command_echo, capture_output=True, text=True)
+	command_echo = f'sudo echo "set superusers={username}" | tee -a /etc/grub.d/40_custom && echo "password_pbkdf2 {username} {password_hash[10]}" | tee -a /etc/grub.d/40_custom'
+	process_echo = subprocess.run(command_echo, capture_output=True, text=True, shell=True)
 	print(process_echo.stdout)
     # Update GRUB
-	process_update = subprocess.run(['update-grub'], capture_output=True, text=True)
+	process_update = subprocess.run(['update-grub'], capture_output=True, text=True, shell=True)
+	print(process_update.stderr)
+	print(process_update.stdout)
 
 
 
